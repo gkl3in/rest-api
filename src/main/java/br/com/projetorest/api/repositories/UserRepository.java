f9d328f4af33ff34951fb97dfa53d41be6110806
Gabriel Klein <gabriel.klein@philips.com>
2024-07-16 18:48:30 -0300

    criado projeto para os testes unitarios

---
+++ src/main/java/br/com/projetorest/api/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.projetorest.api.repositories;
+
+import br.com.projetorest.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
